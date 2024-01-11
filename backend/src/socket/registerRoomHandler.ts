d95cea3baf46df00c50d53af8143ad3b330369a6
Heisenburger <heisenburger10012024@gmail.com>
2024-01-11 19:57:02 +0530

    chore: removed redundant if block

--- backend/src/socket/registerRoomHandler.ts
+++ backend/src/socket/registerRoomHandler.ts
@@ -16,47 +16,45 @@ export function registerRoomHandlers(io: TServer, socket: TSocket) {
 
   async function joinRoom(roomId: number) {
     // check if the user part of other rooms (remove from existing room)
-    if(!hasPermissions()) {
+    if (!hasPermissions()) {
       console.error("error: joinRoom: unauthorized")
       return
     }
     try {
       const room = await roomRepo.getRoom(roomId)
-      if(!room) {
+      if (!room) {
         console.error("error: joinRoom: invalid room")
         return
       }
-      if(room) {
-        const user = await userRepo.get(socket.data.auth?.userId!)
-        socket.data.user = {
-          id: user.id,
-          avatar: user.avatar,
-          username: user.username,
-        }
-
-        // check if the user can join the room (reason: banned, kicked)
-        socket.join(prefixedRoomId(roomId))
-
-        io.emit("room:participant:joined", {
-          roomId,
-          user: socket.data.user,
-          joinedAt: Date.now(),
-        })
+      const user = await userRepo.get(socket.data.auth?.userId!)
+      socket.data.user = {
+        id: user.id,
+        avatar: user.avatar,
+        username: user.username,
       }
-    } catch(err) {
+
+      // check if the user can join the room (reason: banned, kicked)
+      socket.join(prefixedRoomId(roomId))
+
+      io.emit("room:participant:joined", {
+        roomId,
+        user: socket.data.user,
+        joinedAt: Date.now(),
+      })
+    } catch (err) {
       console.log("error: room:join:", err)
     }
   }
 
   function broadcastMessage(message: RoomMessageReq) {
-    if(!hasPermissions()) {
+    if (!hasPermissions()) {
       console.error("error: broadcastMessage: unauthorized")
       return
     }
 
     const socketRoomId = prefixedRoomId(message.roomId)
     const isInRoom = socket.rooms.has(socketRoomId)
-    if(!isInRoom) {
+    if (!isInRoom) {
       console.error("error: broadcastMessage: not in room")
       return
     }
@@ -66,7 +64,7 @@ export function registerRoomHandlers(io: TServer, socket: TSocket) {
       sentAt: Date.now(),
       from: socket.data.user,
       // TODO: validate the incoming `message`
-      content: message.content, 
+      content: message.content,
       roomId: message.roomId,
     }
 
@@ -78,7 +76,7 @@ export function registerRoomHandlers(io: TServer, socket: TSocket) {
     rooms.forEach(room => {
       const segments = room.split(":")
       const roomId = parseInt(segments[1])
-      if(socket.data.user && !isNaN(roomId)) {
+      if (socket.data.user && !isNaN(roomId)) {
         io.emit("room:participant:left", {
           roomId,
           user: socket.data.user,
