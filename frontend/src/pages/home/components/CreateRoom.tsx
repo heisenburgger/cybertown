000adba19b0ae0d204b6f80eed79514994bf202c
Heisenburger <heisenburger10012024@gmail.com>
2024-01-11 19:57:02 +0530

    feat: added react-hook-form for forms

--- frontend/src/pages/home/components/CreateRoom.tsx
+++ frontend/src/pages/home/components/CreateRoom.tsx
@@ -10,14 +10,24 @@ import {
 import {
   Select,
   SelectContent,
-  SelectGroup,
   SelectItem,
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { useState } from "react"
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { useEffect } from "react"
+import { Loader2 } from "lucide-react"
 import { useCreateRoom } from "@/hooks/queries/useCreateRoom"
 import { useMe } from "@/hooks/queries"
 
@@ -26,98 +36,136 @@ type Props = {
   setOpen: (open: boolean) => void
 }
 
-export function CreateRoom(props: Props) {
-  const { data: user } = useMe()
-  const { mutate: createRoomMutate } = useCreateRoom()
-  const { open, setOpen } = props
-  const [topic, setTopic] = useState("")
-  const [language, setLanguage] = useState("")
-  const [maxParticipants, setMaxParticipants] = useState("")
+const newRoomSchema = z.object({
+  topic: z.string().optional(),
+  maxParticipants: z.string(),
+  language: z.string().min(2, {
+   message : "Please select language"
+  })
+})
 
-  function createRoom() {
+export function CreateRoom(props: Props) {
+  const { open, setOpen } = props
+  const { mutate: createRoomMutate, status } =  useCreateRoom()
+  const loading = status === "pending"
+  const { data: user } = useMe()
+
+  const form = useForm<z.infer<typeof newRoomSchema>>({
+    resolver: zodResolver(newRoomSchema),
+    values: {
+      topic: "",
+      maxParticipants: "5",
+      language: "",
+    }
+  })
+
+  // TODO: show alert if this failed
+  function onSubmit(values: z.infer<typeof newRoomSchema>) {
     if(!user) {
       return
     }
     createRoomMutate({
-      topic,
-      language,
-      maxParticipants: parseInt(maxParticipants),
+      ...values,
+      maxParticipants: parseInt(values.maxParticipants),
       metadata: {
         owner: user.id,
         coOwners: [],
       }
     })
-    setLanguage("")
-    setTopic("")
-    setMaxParticipants("")
-    setOpen(false)
   }
+
+  useEffect(() => {
+    if(!open) {
+      form.reset()
+    }
+  }, [open])
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>
         <Button variant="outline">Create Room</Button>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[500px]">
+      <DialogContent className="sm:max-w-[500px]" forceMount>
         <DialogHeader className="mb-4">
           <DialogTitle>Create Room</DialogTitle>
         </DialogHeader>
-        <div className="flex flex-col gap-6">
-          <div className="flex flex-col gap-2">
-            <Label htmlFor="topic">
-              Topic
-            </Label>
-            <Input
-              value={topic}
-              onChange={e => setTopic(e.currentTarget.value)}
-              id="topic"
-              className="px-2 py-2"
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-6">
+            <FormField
+              control={form.control}
+              name="topic"
+              render={({ field }) => {
+                return <FormItem>
+                  <FormLabel>Topic</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Enter topic"
+                      className="px-2 py-2"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              }}
             />
-          </div>
-          <div className="flex gap-4 items-center">
-            <div className="flex flex-col gap-2 flex-1">
-              <Label htmlFor="language">
-                Language
-              </Label>
-              <Select value={language} onValueChange={value => setLanguage(value)}>
-                <SelectTrigger>
-                  <SelectValue placeholder="Select language" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup>
-                    <SelectItem value="english">English</SelectItem>
-                    <SelectItem value="hindi">Hindi</SelectItem>
-                    <SelectItem value="tamil">Tamil</SelectItem>
-                    <SelectItem value="telugu">Telugu</SelectItem>
-                    <SelectItem value="malayalam">Malayalam</SelectItem>
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
+            <div className="flex flex-col sm:flex-row gap-4">
+              <FormField
+                control={form.control}
+                name="language"
+                render={({ field }) => {
+                  return <FormItem className="flex-1">
+                    <FormLabel>Language</FormLabel>
+                    <Select value={field.value} onValueChange={field.onChange}>
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select language" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectItem value="english">English</SelectItem>
+                        <SelectItem value="hindi">Hindi</SelectItem>
+                        <SelectItem value="tamil">Tamil</SelectItem>
+                        <SelectItem value="telugu">Telugu</SelectItem>
+                        <SelectItem value="malayalam">Malayalam</SelectItem>
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                }}
+              />
+              <FormField
+                control={form.control}
+                name="maxParticipants"
+                render={({ field }) => {
+                  return <FormItem className="flex-1">
+                    <FormLabel>Maximum Participants</FormLabel>
+                    <Select value={field.value} onValueChange={field.onChange}>
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select language" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectItem value="1">1</SelectItem>
+                        <SelectItem value="2">2</SelectItem>
+                        <SelectItem value="3">3</SelectItem>
+                        <SelectItem value="4">4</SelectItem>
+                        <SelectItem value="5">5</SelectItem>
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                }}
+              />
             </div>
-            <div className="flex flex-col gap-2 flex-1">
-              <Label htmlFor="participants">
-                Maximum Participants
-              </Label>
-              <Select value={maxParticipants} onValueChange={value => setMaxParticipants(value)}>
-                <SelectTrigger id="participants">
-                  <SelectValue placeholder="Max Participants" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup>
-                    <SelectItem value="1">1</SelectItem>
-                    <SelectItem value="2">2</SelectItem>
-                    <SelectItem value="3">3</SelectItem>
-                    <SelectItem value="4">4</SelectItem>
-                    <SelectItem value="5">5</SelectItem>
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-            </div>
-          </div>
-        </div>
-        <DialogFooter className="mt-4">
-          <Button onClick={createRoom} type="submit">Submit</Button>
-        </DialogFooter>
+            <DialogFooter className="mt-4">
+              <Button disabled={loading} type="submit">
+                {loading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+                <span>Submit</span>
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   )
