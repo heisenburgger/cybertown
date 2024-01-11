dc17373a49d1dae26b3f558b8393384e877f8fdf
Heisenburger <heisenburger10012024@gmail.com>
2024-01-11 19:57:02 +0530

    feat: added editing room inside room

--- frontend/src/pages/room/components/Settings.tsx
+++ frontend/src/pages/room/components/Settings.tsx
@@ -1,12 +1,17 @@
 import { SocketRoom } from "@/types";
 import { WelcomeMessage } from "./WelcomeMessage";
 import { useMe } from "@/hooks/queries";
+import { CreateRoom } from "@/pages/home/components";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Pencil } from "lucide-react";
 
 type Props = {
   room: SocketRoom
 }
 
 export function Settings(props: Props) {
+  const [open, setOpen] = useState(false)
   const { data: user } = useMe()
   const { room } = props
   const welcomeMessage = room.metadata.welcomeMessage ?? ""
@@ -14,7 +19,20 @@ export function Settings(props: Props) {
   const isCoOwner = room.metadata.coOwners?.includes(user?.id as number)
 
   return (
-    <div className="h-full px-2 py-1">
+    <div className="h-full px-2 py-1 flex flex-col gap-5">
+      {isOwner && (
+        <CreateRoom open={open} setOpen={setOpen} mode="edit" roomId={room.id} defaultValues={{
+          topic: room.topic ?? "",
+          maxParticipants: room.maxParticipants.toString(),
+          language: room.language,
+        }}>
+          <Button variant="outline" className="self-end flex gap-2">
+            <span><Pencil strokeWidth="1" size="15" /></span>
+            <span>Edit Room</span>
+          </Button>
+        </CreateRoom>
+      )}
+
       {(isOwner || isCoOwner) && (
         <WelcomeMessage welcomeMessage={welcomeMessage} room={room} />
       )}
